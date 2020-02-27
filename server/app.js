1a8279ff76437a2d9e5f650eabe6c4c0bd375988
Craig Peckett <peckett23@gmail.com>
2020-02-27 20:10:51 +1100

    changed port number

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const history = require("connect-history-api-fallback");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(history());
 app.use(express.static("dist"));
